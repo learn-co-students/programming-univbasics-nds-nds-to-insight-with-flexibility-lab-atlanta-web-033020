bc3b15282ad0da671d86c6ea46e431ebcf59097e
Christina Placide <christina.placide@hotmail.com>
2020-03-24 18:56:58 +0000

    Done.

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -35,13 +35,13 @@ end
 
 def movies_with_director_key(name, movies_collection)
 new_collection = []
- index = 0
+index = 0
   while index < movies_collection.length do 
-     film_artist = movie_with_director_name(name, movies_collection[index])
-     new_collection << film_artist
-     index += 1
-   end  
-   new_collection
+    film_artist = movie_with_director_name(name, movies_collection[index])
+    new_collection << film_artist
+    index += 1
+  end  
+  new_collection
 end
 
 
@@ -49,39 +49,43 @@ def gross_per_studio(collection)
  index = 0
  updated_collection = {} 
  
-  while index < collection.length do 
-     if !updated_collection.has_key?(collection[index][:studio]) # checking if true or false if updated_collection has key in it ? its false so next line it will be true then add key and set to 0.
-      updated_collection[collection[index][:studio]] = 0 # adds key to hash and set value to 0
-      updated_collection[collection[index][:studio]] =  updated_collection[collection[index][:worldwide_gross]] #looks at updated looks at the key then set value = to w_g
-     else 
-     updated_collection[collection[index][:studio] = updated_collection[collection[index][:worldwide_gross]] 
- # binding.pry
+   while index < collection.length do 
+   # binding.pry 
+     if !updated_collection.has_key?(collection[index][:studio]) 
+        updated_collection[collection[index][:studio]] = 0 
+        
+        updated_collection[collection[index][:studio]] =
+        collection[index][:worldwide_gross]
+      else 
+        updated_collection[collection[index][:studio]] +=
+        collection[index][:worldwide_gross]
+    end 
+     index += 1
     end
-    index += 1
-  end
-     updated_collection
- end
-
-
-def movies_with_directors_set(source)
-  # GOAL: For each director, find their :movies Array and stick it in a new Array
-  #
-  # INPUT:
-  # * source: An Array of Hashes containing director information including
-  # :name and :movies
-  #
-  # RETURN:
-  #
-  # Array of Arrays containing all of a director's movies. Each movie will need
-  # to have a :director_name key added to it.
+ 
+ updated_collection
 end
+
+
+def movies_with_directors_set(source) #(source)an array of hashes containing director info :name :movies
+index = 0
+set_source = [] # AoA containing source and all :movies and :director_name key
+ while index < source.length do
+  # binding.pry             #source[index][movies]=movies array w/{} of titles
+    dir_name_key = source[index][:name] 
+    dir_movie_key = source[index][:movies]
+    set_source.push(movies_with_director_key(dir_name_key, dir_movie_key))
+   index += 1 
+   end
+   set_source
+ end
 
 # ----------------    End of Your Code Region --------------------
 # Don't edit the following code! Make the methods above work with this method
 # call code. You'll have to "see-saw" to get this to work!
 
-def studios_totals(nds)
-  a_o_a_movies_with_director_names = movies_with_directors_set(nds)
-  movies_with_director_names = flatten_a_o_a(a_o_a_movies_with_director_names)
-  return gross_per_studio(movies_with_director_names)
-end
+ def studios_totals(nds)
+   a_o_a_movies_with_director_names = movies_with_directors_set(nds)
+   movies_with_director_names = flatten_a_o_a(a_o_a_movies_with_director_names)
+   return gross_per_studio(movies_with_director_names)
+ end
